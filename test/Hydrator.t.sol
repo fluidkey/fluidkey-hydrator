297c1a5ab731947b606179f9de840ce61bae03d9
moritz <moritz@fluidkey.com>
2024-02-16 23:38:09 +0300

    optimize and clean up

---
+++ test/Hydrator.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Hydrator} from "../src/Hydrator.sol";
+
+contract CounterTest is Test {
+    Hydrator public hydrator;
+
+    function setUp() public {
+        hydrator = new Hydrator();
+    }
+
+    function test_callOtherContract() public {
+        hydrator.deploySafe(hex"D2661728b35916D0A15834c558D4e6E3b7567f76");
+    }
+}
